162bc5d15e4682a86fbb375dfee45e2f6b607a3e
Austin Papritz <austin@papritz.dev>
2023-01-23 15:45:44 -0800

    home page added which links to books page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail.js';
 import NotFound from './components/notfound/NotFound.js';
+import Home from './components/home/Home.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
+
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={BookList} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/books" component={BookList} />
           <Route exact path="/books/:id" component={BookDetail} />
-          {/* <Route path="*" component={NotFound} /> */}
+          <Route path="*" component={NotFound} />
         </Switch>
       </BrowserRouter>
     </main>
