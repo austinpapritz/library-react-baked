29b99616e7ce70214591b0bfdcba7190e5e91ac9
Austin Papritz <austin@papritz.dev>
2023-01-23 15:11:42 -0800

    useParams grabs added to BookDetail but for some reason id is undefined

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,13 +1,18 @@
 import Author from '../author/Author';
 import './Book.css';
 
+import { Link } from 'react-router-dom';
+
 function Book({
   book: { title, total_pages: totalPages, rating, isbn, published_date: publishedDate, authors },
   showDetail = false,
+  book_id,
 }) {
   return (
     <article className="book">
-      <h3>{title}</h3>
+      <Link to={`/details/${book_id}`}>
+        <h3>{title}</h3>
+      </Link>
       <address>
         {authors.map((author) => (
           <Author key={author.author_id} author={author} />
